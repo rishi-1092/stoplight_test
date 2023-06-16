a7f8d277c630c7a63d7747a124538e86e08533a0
Rishi Reddy <rishireddit@gmail.com>
2023-06-16 17:45:17 +0530

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNjIwOQ/spectral.js?branch_slug=master&token=6aa24c58-53c7-45e2-933c-53a7c8776676";
+export default { extends: ruleset };
